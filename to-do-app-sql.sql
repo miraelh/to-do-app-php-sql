ead73584e94c0651e6d793d9fde73c28f566f6cc
Mira E <miraelhajj0@gmail.com>
2025-04-19 16:44:34 +0300

    Creating the tasks Table

--- to-do-app-sql.sql
+++ to-do-app-sql.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash VARCHAR(255) NOT NULL
 );
 
+
 CREATE TABLE TodoLists (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -12,3 +13,17 @@ CREATE TABLE TodoLists (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
+
+
+CREATE TABLE Tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    todo_list_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    status ENUM('pending', 'done', 'in-progress') DEFAULT 'pending',
+    due_date DATE,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (todo_list_id) REFERENCES TodoLists(id) ON DELETE CASCADE
+);
+
+
